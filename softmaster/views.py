bff2e59a951445e605052ad731f2ed79014889f9
kshitij <kshitijbisen36@gmail.com>
2023-01-26 19:15:27 +0530

    demo1.py file added

--- softmaster/views.py
+++ softmaster/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,20 +6,22 @@ from .models import Color
 from .serializers import ColorSerializer
 # Create your views here.
 
+
 class ColorView(APIView):
-    def get(self,request):
+    def get(self, request):
         colors = Color.objects.all()
-        serializer = ColorSerializer(colors,many=True)
+        serializer = ColorSerializer(colors, many=True)
         if serializer is not None:
-            return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class NonDeletedColorView(APIView):
-    def get(self,request):
+    def get(self, request):
         colors = Color.color_obj.all()
-        serializer = ColorSerializer(colors,many=True)
+        serializer = ColorSerializer(colors, many=True)
         if serializer is not None:
-            return Response(serializer.data,status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         else:
-            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
